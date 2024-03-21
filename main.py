d46bd351615eb3428a6ae2d9d6c8180adc72dd34
Blonde_Marisa <blondemarisa@gmail.com>
2024-03-21 19:16:48 +0800

    完成发布

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-# main.py 文件的内容修改为
-import os
-os.system('python Function-22371063.py')
-=======
 import os
 os.system('python Function-22373173.py')
 //Confict Solved
->>>>>>> f04118a765a09a0dda93cf9b8f04c39cecc287f7
